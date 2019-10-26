007902cc971ed433adc9acc9a1f4d165404a0737
Hanna Lee <mani21na@gmail.com>
2019-10-26 07:38:28 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,17 +2,17 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
   def self.table_name
-    slef.to_s.downcase.pluralize
+    self.to_s.downcase.pluralize
   end
-  
-  def column_names
-    DB[:conn].results_as_hash = true 
-    
-    sql = "paragma table_info('#{table_name}') "
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{table_name}')"
+
     table_info = DB[:conn].execute(sql)
-    
     column_names = []
     table_info.each do |row|
       column_names << row["name"]
@@ -25,13 +25,13 @@ class InteractiveRecord
       self.send("#{property}=", value)
     end
   end
-  
+
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
-  
+
   def table_name_for_insert
     self.class.table_name
   end
@@ -47,9 +47,10 @@ class InteractiveRecord
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
-  
-  def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-    DB[:conn].execute(sql, name)
-  end
+
+def self.find_by_name(name)
+  sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+  DB[:conn].execute(sql, name)
+end
+
 end
