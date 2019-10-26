30b3c667776b085957174f8622bcb84e63ab09f7
Hanna Lee <mani21na@gmail.com>
2019-10-26 07:28:38 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -11,8 +11,14 @@ class InteractiveRecord
     DB[:conn].results_as_hash = true 
     
     sql = "paragma table_info('#{table_name}') "
+    table_info = DB[:conn].execute(sql)
     
-    
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
   end
 
+  
 end
