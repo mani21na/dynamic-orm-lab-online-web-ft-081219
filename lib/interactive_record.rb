76d093c6fb2cd96b7b02f82ee642b22d9f5d2081
Hanna Lee <mani21na@gmail.com>
2019-10-26 07:22:41 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,9 +2,10 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  
   def self.table_name
     slef.to_s.downcase.pluralize
-  end 
+  end
   
   def column_names
     DB[:conn].results_as_hash = true 
